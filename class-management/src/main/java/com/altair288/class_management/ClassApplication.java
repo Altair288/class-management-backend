b41b1d7e53a7b630a5924fd31e988e60f627b241
Altair288 <1755529573@qq.com>
2025-02-28 11:45:24 +0800

    Update Main Interface && Add Sql Connection

---
+++ class-management/src/main/java/com/altair288/class_management/ClassApplication.java
@@ -0,0 +1,11 @@
+package com.altair288.class_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassApplication.class, args);
+    }
+}
